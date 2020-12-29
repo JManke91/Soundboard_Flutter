196f6bfc3187771891301caea86dcdf253740a73
Julian Manke <Julian.Manke@jambit.com>
2020-12-29 16:41:55 +0100

    button functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -7,12 +11,46 @@ void main() {
 }
 
 class Home extends StatelessWidget {
+
+  AudioCache cache = new AudioCache();
+  AudioPlayer player;
+
+  void _play(String argument) async{
+    _stopFile();
+    player = await cache.play(argument);
+  }
+
+  void _stopFile() {
+    player?.stop();
+  }
+
+  /// TODO: Make private
+  void kaffeeButtonPressed() {
+    _play('andreas_kaffee.mp3');
+  }
+
+  void haltStopButtonPressed() {
+    _play('andreas_halt_stop.mp3');
+  }
+
+  void quatschButtonPressed() {
+    _play('quatsch.mp3');
+  }
+
+  void vielleichtWahrButtonPressed() {
+    _play('x_faktor_vielleicht_wahr.mp3');
+  }
+
+  void freiErfundenButtonPressed() {
+    _play('x_faktor_geschichte_frei_erfunden.mp3');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text('TopBar title'),
+          title: Text('Soundboard'),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -23,14 +61,14 @@ class Home extends StatelessWidget {
               children: [
                 RaisedButton(
                     onPressed: () {
-                      // do something
+                      kaffeeButtonPressed();
                     },
-                    child: Text('first')),
+                    child: Text('Andreas Kaffee')),
                 RaisedButton(
                     onPressed: () {
-                      // do something
+                      haltStopButtonPressed();
                     },
-                    child: Text('second')),
+                    child: Text('Halt STOP')),
               ],
             ),
             Row(
@@ -39,14 +77,26 @@ class Home extends StatelessWidget {
               children: [
                 RaisedButton(
                     onPressed: () {
-                      // do something
+                      quatschButtonPressed();
                     },
-                    child: Text('third')),
+                    child: Text('Quatsch')),
                 RaisedButton(
                     onPressed: () {
-                      // do something
+                      vielleichtWahrButtonPressed();
                     },
-                    child: Text('fourth')),
+                    child: Text('Vielleicht wahr')),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                RaisedButton(
+                  onPressed: () {
+                    freiErfundenButtonPressed();
+                  },
+                  child: Text('Frei erfunden'),
+                ),
               ],
             )
           ],
